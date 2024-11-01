f00c4f1aab6e7715a32b7294e0da4b6723f7e8a0
Avisekh Biswakarma <avishek.viswakarma@gmail.com>
2024-11-01 23:44:30 +0530

    first commit

---
+++ test/index.test.js
@@ -0,0 +1,6 @@
+const { binarySearch } = require('../src/index');
+
+// Simple test case
+const array = [1, 3, 5, 7, 9, 11];
+const target = 7;
+console.log(binarySearch(array, target)); // Should print 3
